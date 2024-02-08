5b168f58a8665fa4f82a9f61b789e7f9164445b5
vaha75355@gmail.com <vaha75355@gmail.com>
2024-02-08 22:44:15 +0400

    second commit

---
+++ day 2/homework2.py
@@ -0,0 +1,18 @@
+print("gavicani da davumegobrdi")
+
+#nia khoperia
+#vano motiashvili
+#nika keshelava
+#barbare tinikashvili
+#sandro razmadze
+
+print("ras fiqrob GOA-ze")
+
+#me vfiqrob rom goas sheudzlia gaaumjobesos 
+#chemi comunikaciis agretve programirebis unarebi
+#da mascavlos ra mchirdeba imistvis rom kargi programisti gamvide
+#kidev didi madlobeli var rom chemma mshoblebma
+#momces sashualeba rom goashi shemovsulikavi  
+
+
+
