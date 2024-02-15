58bae2e9ff4258063a710be051b8de05d41e0ed1
nodarigrigalashvili <vaha75355@gmail.com>
2024-02-15 17:23:38 +0400

    third commit

---
+++ day 3/homework3.py
@@ -0,0 +1,17 @@
+dedachemis_wlovaneba=51
+print(dedachemis_wlovaneba)
+
+dedachemis_wlovaneba2=dedachemis_wlovaneba + 2
+print(dedachemis_wlovaneba2)
+
+mamachemis_wlovaneba=54
+print(mamachemis_wlovaneba)
+
+mamachemis_wlovaneba2=mamachemis_wlovaneba + 2
+print(mamachemis_wlovaneba2)
+
+chemi_dis_wlovaneba=14
+print(chemi_dis_wlovaneba)
+
+chemi_dis_wlovaneba2=chemi_dis_wlovaneba + 2
+print(chemi_dis_wlovaneba2)
