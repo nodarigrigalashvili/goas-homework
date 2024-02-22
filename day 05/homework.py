60290b07002fd145e1ad333d42e0be7e0622ba2c
nodarigrigalashvili <vaha75355@gmail.com>
2024-02-22 18:27:40 +0400

    fifth commit

---
+++ day 05/homework.py
@@ -0,0 +1,11 @@
+number1 = int(input("name your number: "))
+number2 = int(input("name your number: "))
+
+print(number1 * number2)
+print(number1 + number2)
+print(number1 - number2)
+print(number1 / number2)
+print(number1 > number2)
+print(number1 < number2)
+print(number1 == number2)
+
