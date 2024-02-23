ab98fe63848f7950113ac4003af7908708f55cd2
nodarigrigalashvili <vaha75355@gmail.com>
2024-02-23 18:10:16 +0400

    fifth commit

--- day 5/homework.py
+++ day 5/homework.py
@@ -7,5 +7,7 @@ print(number1 - number2)
 print(number1 / number2)
 print(number1 > number2)
 print(number1 < number2)
+print(number1 <= number2)
+print( number1 >= number2)
 print(number1 == number2)
 
---
+++ day 6/classwork.py
@@ -0,0 +1,5 @@
+name = input("your firstname: ")
+lastname = input("your lastname: ")
+age = int(input("your age: "))
+
+print(f"my name is {name} {lastname} and my age is {age}.")
