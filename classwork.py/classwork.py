3a1050e4cf3d6da2096db3d041c207c933161253
nodarigrigalashvili <vaha75355@gmail.com>
2024-03-03 19:31:40 +0400

    seventh commit

---
+++ classwork.py/classwork.py
@@ -0,0 +1,8 @@
+age = int(input("what is your age?: "))
+
+if age >= 18:
+    print("youre an adult")
+else:
+    print("youre not an adult")
+
+    
---
+++ day 8/homework.py
@@ -0,0 +1,11 @@
+name = input("Your name: ")
+password = input("your password: ")
+repeat_password = ("your password and or your username is incorrect")
+
+if name == "nodo":
+    if password == "bubbler":
+        print("registered successfully")
+else:
+    print(repeat_password)
+    
+
