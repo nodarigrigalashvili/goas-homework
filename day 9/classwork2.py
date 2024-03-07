cd2a114424e76bda8e510b03e8c22ee73f88620a
nodarigrigalashvili <vaha75355@gmail.com>
2024-03-07 18:07:16 +0400

    eighth commit

---
+++ day 9/classwork2.py
@@ -0,0 +1,3 @@
+numb = int(input("name your number: "))
+
+print(numb % 5 == 0 and numb % 10 == 0)
---
+++ day 9/homework.py
@@ -0,0 +1,12 @@
+num = int(input("Enter Number: "))
+
+
+print(num % 2 == 0 and num % 4 == 0)
+
+
+print(7 > 4)
+print(23 < 130)
+print(392 >= 292)
+print(524 <= 500)
+print(100 == 100.0)
+print(10.00 != 10.0)
