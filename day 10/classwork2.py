8601731024109ce77797b36836017a9ca45b316e
nodarigrigalashvili <vaha75355@gmail.com>
2024-03-10 15:55:10 +0400

    ninth commit

---
+++ day 10/classwork2.py
@@ -0,0 +1,10 @@
+series = ["breaking bad", "the boys", "better call saul"]
+
+print(series)
+
+print(series[1])
+
+Numbers = [352, 2134, 421, 122, 143, 124, 4323,]
+
+print(Numbers[6])
+print(Numbers[5], [3])
---
+++ day 10/homework.py
@@ -0,0 +1,26 @@
+programming_languages = ["python", "javascript", "c++", "Html", "PHP"]
+
+print(programming_languages)
+
+print(programming_languages[4])
+print(programming_languages[3])
+print(programming_languages[2])
+
+Chaos = ["bob", 3135, 341.43, 123 == 312]
+
+print(Chaos[0])
+print(Chaos[1])
+print(Chaos[2])
+print(Chaos[3])
+
+num = [0, 4, 8, 12, 16, 20]
+
+print(num[5])
+
+num2 = [31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+
+print(num2[0] + num2[1] + num2[2])
+
+numnum = [30, 34, 38, 42, 46, 50]
+
+print(numnum[0] + numnum[1] + numnum[2])
