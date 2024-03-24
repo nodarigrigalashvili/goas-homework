c66c0287bccc1095fdfce78cc7397500934d0b4f
nodarigrigalashvili <vaha75355@gmail.com>
2024-03-24 19:53:04 +0400

    eleventh commit

---
+++ day 13/classwork.py
@@ -0,0 +1,7 @@
+list = ["blue", 1, 0.2, "wow", "yes"]
+
+print(list[0])
+
+print(list[4])
+
+print(list[0:4])
---
+++ day 13/homework.py
@@ -0,0 +1,4 @@
+list = ["purple", True, 234, "lol", 0.0001]
+
+for i in list:
+    print(i)
