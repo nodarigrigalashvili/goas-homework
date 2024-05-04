445e97af54c0f51818a1ef4c806b65c2d26594d3
nodarigrigalashvili <vaha75355@gmail.com>
2024-05-05 00:39:19 +0400

    twenty first commit

--- day 24/classwork/experiment.py
+++ day 24/classwork/experiment.py
@@ -1,9 +1,8 @@
-
-Listofnumbers = [1,1,1,1,3,4,5,6]
+Listofnumbers = [1,1,1,1,1,3,4,5,6]
 world = 0
-lol = 0
+lol = 1
 for i in Listofnumbers:
     if i == world:
         lol = lol + 1
-    world = world + i
+    world = i
 print(lol)
--- day 24/homework.py
+++ day 24/homework.py
@@ -34,15 +34,11 @@ for i in name:
 print(heya2)
 
 
-list2 = [1,1,1,1,3,4,5,6]
-
-    
-
-
-     
-
-    
-
-
-
-    
+Listofnumbers = [1,1,1,1,1,3,4,5,6]
+world = 0
+lol = 1
+for i in Listofnumbers:
+    if i == world:
+        lol = lol + 1
+    world = i
+print(lol)
